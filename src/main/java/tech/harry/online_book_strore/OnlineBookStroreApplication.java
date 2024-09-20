4bf2403316e3b445d00843b6711f06a8dd707137
Vinay Prakash Yadav <vinayprakashyadav8364@gmail.com>
2024-09-20 16:40:50 +0530

    added

--- src/main/java/tech/harry/online_book_strore/OnlineBookStroreApplication.java
+++ src/main/java/tech/harry/online_book_strore/OnlineBookStroreApplication.java
@@ -8,7 +8,7 @@ public class OnlineBookStroreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineBookStroreApplication.class, args);
-		System.out.println("this is online_store2 ");
+		System.out.println("this is online_store2....... ");
 	}
 
 }
