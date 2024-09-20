4cbf5d5fd2eefb4db110f0991da1b5e0809eb662
Vinay Prakash Yadav <vinayprakashyadav8364@gmail.com>
2024-09-20 15:51:42 +0530

    Initial commit

---
+++ src/main/java/tech/harry/online_book_strore/OnlineBookStroreApplication.java
@@ -0,0 +1,13 @@
+package tech.harry.online_book_strore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStroreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStroreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/harry/online_book_strore/OnlineBookStroreApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.harry.online_book_strore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStroreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
