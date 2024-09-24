8ff2703cf3c3bb7ba7edb140926edf5ef56e357b
VinayPrakashYadav <125681695+v1nayHyder@users.noreply.github.com>
2024-09-24 08:16:50 +0530

    Update OnlineBookStroreApplication.java

--- src/main/java/tech/harry/online_book_strore/OnlineBookStroreApplication.java
+++ src/main/java/tech/harry/online_book_strore/OnlineBookStroreApplication.java
@@ -11,9 +11,7 @@ public class OnlineBookStroreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineBookStroreApplication.class, args);
-		System.out.println("this is online_store2....... ");
-		System.out.println("this is online book store application");
-		System.out.println("recursive use");
+		
 	}
 	@Bean
 	public PasswordEncoder passwordEncoder() {
